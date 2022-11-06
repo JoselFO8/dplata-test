0afddb615e1b017a65b3d8fc4100a908ab177cb5
Jose <joseluisfo_@hotmail.com>
2022-11-05 23:42:58 -0500

    The client-profile component is now styled in the bar container

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -4,7 +4,6 @@ import { SignUpComponent } from "./sign-up/sign-up.component";
 
 const routes: Routes = [
     {
-        // path: INTERNAL_PATHS.AUTH_LOGIN,
         path: 'signup',
         component: SignUpComponent,
     }
